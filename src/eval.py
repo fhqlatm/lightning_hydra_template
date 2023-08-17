7b39601c8ddc306b4c19ecc4e82862a616d84329
ubuntu <fhqlatm@gmail.com>
2023-08-17 10:46:23 +0900

    add evaluation code

---
+++ src/eval.py
@@ -0,0 +1,112 @@
+import os
+import json
+import hydra
+from pathlib import Path
+from typing import List, Optional, Tuple
+
+import lightning as L
+import pyrootutils
+import torch
+from lightning import Callback, LightningDataModule, LightningModule, Trainer
+from lightning.pytorch.callbacks.lr_monitor import LearningRateMonitor
+from lightning.pytorch.callbacks.model_checkpoint import ModelCheckpoint
+from lightning.pytorch.loggers import Logger, TensorBoardLogger, WandbLogger
+from omegaconf import DictConfig
+
+pyrootutils.setup_root(__file__, indicator=".project-root", pythonpath=True)
+# ------------------------------------------------------------------------------------ #
+# the setup_root above is equivalent to:
+# - adding project root dir to PYTHONPATH
+#       (so you don't need to force user to install project as a package)
+#       (necessary before importing any local modules e.g. `from src import utils`)
+# - setting up PROJECT_ROOT environment variable
+#       (which is used as a base for paths in "configs/paths/default.yaml")
+#       (this way all filepaths are the same no matter where you run the code)
+# - loading environment variables from ".env" in root dir
+#
+# you can remove it if you:
+# 1. either install project as a package or move entry files to project root dir
+# 2. set `root_dir` to "." in "configs/paths/default.yaml"
+#
+# more info: https://github.com/ashleve/pyrootutils
+# ------------------------------------------------------------------------------------ #
+
+from src import utils
+from src.models.LightningModule import LightningModule
+from src.dataset.LightningDataModule import LihgningDataModule
+
+log = utils.get_pylogger(__name__)
+
+
+@utils.task_wrapper
+def evaluate(_config: DictConfig) -> Tuple[dict, dict]:
+    """Evaluates given checkpoint on a datamodule testset.
+
+    This method is wrapped in optional @task_wrapper decorator, that controls the behavior during
+    failure. Useful for multiruns, saving info about the crash, etc.
+
+    Args:
+        _config (DictConfig): Configuration composed by Hydra.
+
+    Returns:
+        Tuple[dict, dict]: Dict with metrics and dict with all instantiated objects.
+    """
+
+    assert _config.ckpt_path
+
+    log.info(f"Instantiating datamodule...")
+
+    log.info(f"Instantiating datamodule...")
+    datamodule: LightningDataModule = LihgningDataModule(_config=_config.data)
+
+    log.info(f"Instantiating model...")
+    model: LightningModule = LightningModule(_config=_config.model)
+
+    log.info("Instantiating loggers...")
+    tensorboard_logger = TensorBoardLogger(**_config.logger.tensorboard)
+    logger: List[Logger] = [tensorboard_logger, ]
+
+    log.info(f"Instantiating trainer...")
+    trainer: Trainer = Trainer(**_config.trainer, logger=logger)
+
+    object_dict = {
+        "_config": _config,
+        "datamodule": datamodule,
+        "model": model,
+        "logger": logger,
+        "trainer": trainer,
+    }
+
+    if logger:
+        log.info("Logging hyperparameters!")
+        utils.log_hyperparameters(object_dict)
+
+    log.info("Starting testing!")
+    trainer.test(
+        model=model, 
+        datamodule=datamodule, 
+        ckpt_path=_config.ckpt_path
+    )
+
+    # for predictions use trainer.predict(...)
+    # predictions = trainer.predict(model=model, dataloaders=dataloaders, ckpt_path=_config.ckpt_path)
+
+    metric_dict = trainer.callback_metrics
+
+    return metric_dict, object_dict
+
+
+@hydra.main(version_base="1.3", config_path="../configs", config_name="eval.yaml")
+def main(_config: DictConfig) -> None:
+    # Set CUDA devices
+    os.environ["CUDA_VISIBLE_DEVICES"] = _config["device_ids"]
+    
+    # apply extra utilities
+    # (e.g. ask for tags if none are provided in _config, print _config tree, etc.)
+    utils.extras(_config)
+
+    evaluate(_config)
+
+
+if __name__ == "__main__":
+    main()
